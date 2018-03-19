e1325afa494738ea8406acfbcb5b89c1a4941dc3
Wan Hao Jun <wanhaojun118@hotmail.com>
2018-03-19 10:55:39 +0800

    change budget system and superblock start block

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -77,11 +77,11 @@ public:
         consensus.nMasternodePaymentsIncreaseBlock = 158000; // actual historical value (OBSOLETE)
         consensus.nMasternodePaymentsIncreasePeriod = 576*30; // 17280 - actual historical value (OBSOLETE)
         consensus.nInstantSendKeepLock = 24; // ATTENTION
-        consensus.nBudgetPaymentsStartBlock = 328008; // actual historical value// ATTENTION
+        consensus.nBudgetPaymentsStartBlock = 1000; // old value is 328008 // actual historical value// ATTENTION
         consensus.nBudgetPaymentsCycleBlocks = 16616; // ~(60*24*30)/2.6, actual number of blocks per month is 200700 / 12 = 16725// ATTENTION
         consensus.nBudgetPaymentsWindowBlocks = 100;// ATTENTION
         consensus.nBudgetProposalEstablishingTime = 60*60*24;
-        consensus.nSuperblockStartBlock = 614820; // The block at which 12.1 goes live (end of final 12.0 budget cycle)
+        consensus.nSuperblockStartBlock = 1000; // old value is 614820 // The block at which 12.1 goes live (end of final 12.0 budget cycle)
         consensus.nSuperblockCycle = 16616; // ~(60*24*30)/2.6, actual number of blocks per month is 200700 / 12 = 16725
         consensus.nGovernanceMinQuorum = 10;
         consensus.nGovernanceFilterElements = 20000;
