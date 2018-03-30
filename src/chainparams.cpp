362fdf5a57cb2886533187a78efe1e15633e4b97
Wan Hao Jun <wanhaojun118@hotmail.com>
2018-03-30 11:43:07 +0800

    renew pszTimestamp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -51,7 +51,7 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
  */
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
-    const char* pszTimestamp = "29/Mar/2018 North, South Korea fix April date for first summit in years";
+    const char* pszTimestamp = "30/Mar/2018 Ads pulled from Ingraham show after she mocked Parkland survivor";
     const CScript genesisOutputScript = CScript() << ParseHex("0429e80e544f30d71c3c5b99c04ce6a15d751a86f4b3815583da37c658086bed18357f7e54d56a7fd63f5b1d660034b9b56ec4378f041dfd19b2bef5e076c91e5a") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
